9ec60674b1fd3681a563a4a468827414055f1f96
Maine <157335221+jaterra@users.noreply.github.com>
2024-02-13 17:48:18 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+
+df = pd.read_csv('big-mac-full-index.csv')
+
+for i, r in df.iterrows():
+    print(r['date'],
+          r['iso_a3'])
+    
+
+print(df.apply(lambda row: row['date'], axis=1))
+print(df.apply(lambda row: row['iso_a3'], axis=1))
